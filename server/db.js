a35e9c4fafb7e80c52b48a13efffc11c1dfd25fa
pwnsoni <pawansoni76@gmail.com>
2022-09-24 05:05:29 +0530

    Small changes

--- server/db.js
+++ server/db.js
@@ -3,7 +3,10 @@ const dbUri = process.env.DB_CONNECTION_URL;
 
 const dbUtil = {
     connect: async () => {
+        console.log("In DB")
         try {
+            console.log("In try")
+            console.log(dbUri[1])
             await mongoose.connect(dbUri);
             console.log(`Successfully connected to DB`)
         } catch (error) {
